a8459ac49cad1b40c03a4585c0753fec37a66910
Marcus <codeninjasstouffville@outlook.com>
2025-03-26 19:01:55 -0400

    Update
    sdgdfhdfrf

---
+++ Assets/Scripts/ObstacleDeath.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ObstacleDeath : MonoBehaviour
+{
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject);
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            Debug.Log("You Lose");
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
