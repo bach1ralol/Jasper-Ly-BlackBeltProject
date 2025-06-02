fbbb8f9a25647fb408da7a132dc15bcd367b0867
Marcus <codeninjasstouffville@outlook.com>
2025-06-02 19:06:30 -0400

    asldkjflasdjf

--- Assets/Scripts/Play.cs
+++ Assets/Scripts/Play.cs
@@ -6,18 +6,21 @@ public class PlayAgain : MonoBehaviour
     public void Play()
     {
         SceneManager.LoadScene("MainGame");
+        FindFirstObjectByType<AudioManager>().Stop("MainMenuMusic");
         FindFirstObjectByType<AudioManager>().Play("ThemeSong");
     }
 
     public void Shop()
     {
         SceneManager.LoadScene("Shop");
+        FindFirstObjectByType<AudioManager>().Stop("MainMenuMusic");
         FindFirstObjectByType<AudioManager>().Play("ShopMusic");
     }
 
     public void Credits()
     {
         SceneManager.LoadScene("Credits");
+        FindFirstObjectByType<AudioManager>().Stop("MainMenuMusic");
         FindFirstObjectByType<AudioManager>().Play("CreditsMusic");
     }
 
@@ -27,6 +30,14 @@ public class PlayAgain : MonoBehaviour
         {
             SceneManager.LoadScene("MainMenu");
             FindFirstObjectByType<AudioManager>().Stop("ThemeSong");
+        }
+    }
+
+    public void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "MainMenu")
+        {
+            FindFirstObjectByType<AudioManager>().Play("MainMenuMusic");
         }
     }
 }
