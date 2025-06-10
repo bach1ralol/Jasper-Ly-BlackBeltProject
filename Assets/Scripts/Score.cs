a4dc51a188a79018549d2b97a86ba53da0d476f5
Marcus <codeninjasstouffville@outlook.com>
2025-06-10 18:01:46 -0400

    asdfasdfasdf

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 using TMPro;
 public class Score : MonoBehaviour
 {
+
+
+    public int score;
     private float timeElapsed = 0f;
     private bool timerIsRunning = true;
     public DestroyStart destroyStart;
@@ -12,6 +15,7 @@ public class Score : MonoBehaviour
         if (timerIsRunning && destroyStart == null)
         {
             timeElapsed += Time.deltaTime;
+            score =  (int)timeElapsed;
             text.text = ((int)timeElapsed).ToString();
         }
     }
