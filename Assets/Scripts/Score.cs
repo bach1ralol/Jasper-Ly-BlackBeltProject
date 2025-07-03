c4b28789aabb3231e569ddd7afb3a029e9382302
Marcus <codeninjasstouffville@outlook.com>
2025-07-03 17:57:46 -0400

    FINAL UPDATES!!!

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,34 +1,43 @@
 using UnityEngine;
 using TMPro;
-using System.Runtime.InteropServices;
+
 public class Score : MonoBehaviour
 {
-
-
     public int score;
     private float timeElapsed = 0f;
     private bool timerIsRunning = true;
+
     public DestroyStart destroyStart;
     public TMP_Text text;
+    public TMP_Text highScoreText;
 
     private int highScore = 0;
-    public Text highScoreText;
-    private bool beatHighScore;
-
-    private void Update()
-    {
-        if (timerIsRunning && destroyStart == null)
-        {
-            timeElapsed += Time.deltaTime;
-            score =  (int)timeElapsed;
-            text.text = ((int)timeElapsed).ToString();
-        }
-    }
+    private bool beatHighScore = false;
 
     private void Start()
     {
-        highScore = PlayerPrefs.GetInt("HighScore");
+        // Load and display saved high score
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
         highScoreText.text = "High Score: " + highScore.ToString();
     }
-}
 
+    private void Update()
+    {
+        
+        if (timerIsRunning && destroyStart == null)
+        {
+            timeElapsed += Time.deltaTime;
+            score = (int)timeElapsed;
+            text.text = score.ToString();
+
+            
+        if (score > highScore)
+        {
+                highScore = score;
+                PlayerPrefs.SetInt("HighScore", highScore);
+                PlayerPrefs.Save();
+                highScoreText.text = "High Score: " + highScore.ToString();
+        }
+        }
+    }
+}
